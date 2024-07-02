5627aadcdd9c24d827a1b6e118f2795e4e4872e6
jiaruili <jiarui li>
2024-07-02 10:21:31 +0200

    second commit

--- test.py
+++ test.py
@@ -1,50 +1,88 @@
 import os
-import torch
 import numpy as np
-from skimage import color, io
-from skimage.transform import resize
-from torch.utils.data import Dataset, DataLoader
-import torchvision.transforms as transforms
+from options.test_options import TestOptions
+from data import create_dataset
+from models import create_model
+from util.visualizer import save_images
+from util import html
+from PIL import Image
 
-class TestDataset(Dataset):
-    def __init__(self, root_dir, transform=None):
-        self.root_dir = root_dir
-        self.transform = transform
-        self.image_files = [f for f in os.listdir(root_dir) if os.path.isfile(os.path.join(root_dir, f))]
+try:
+    import wandb
+except ImportError:
+    print('Warning: wandb package cannot be found. The option "--use_wandb" will result in error.')
 
-    def __len__(self):
-        return len(self.image_files)
+if __name__ == '__main__':
+    opt = TestOptions().parse()  # get test options
+    # hard-code some parameters for test
+    opt.num_threads = 0   # test code only supports num_threads = 0
+    opt.batch_size = 1    # test code only supports batch_size = 1
+    opt.serial_batches = True  # disable data shuffling; comment this line if results on randomly chosen images are needed.
+    opt.no_flip = True    # no flip; comment this line if results on flipped images are needed.
+    opt.display_id = -1   # no visdom display; the test code saves the results to a HTML file.
+    dataset = create_dataset(opt)  # create a dataset given opt.dataset_mode and other options
+    model = create_model(opt)      # create a model given opt.model and other options
+    model.setup(opt)               # regular setup: load and print networks; create schedulers
 
-    def __getitem__(self, idx):
-        img_name = os.path.join(self.root_dir, self.image_files[idx])
-        image = io.imread(img_name)
-        image = resize(image, (256, 256))
-        gray_image = color.rgb2gray(image)
-        gray_image = np.expand_dims(gray_image, axis=2)
-        if self.transform:
-            gray_image = self.transform(gray_image)
-        return gray_image, img_name
+    # initialize logger
+    if opt.use_wandb:
+        wandb_run = wandb.init(project=opt.wandb_project_name, name=opt.name, config=opt) if not wandb.run else wandb.run
+        wandb_run._label(repo='CycleGAN-and-pix2pix')
 
-transform = transforms.Compose([
-    transforms.ToTensor(),
-])
+    # create a website
+    web_dir = os.path.join(opt.results_dir, opt.name, '{}_{}'.format(opt.phase, opt.epoch))  # define the website directory
+    if opt.load_iter > 0:  # load_iter is 0 by default
+        web_dir = '{:s}_iter{:d}'.format(web_dir, opt.load_iter)
+    print('creating web directory', web_dir)
+    webpage = html.HTML(web_dir, 'Experiment = %s, Phase = %s, Epoch = %s' % (opt.name, opt.phase, opt.epoch))
 
-test_dataset = TestDataset(root_dir='datasets/my_dataset/test', transform=transform)
-test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False)
+    # Initialize an empty list to store the output images
+    output_images = []
 
-model = torch.load('checkpoints/your_dataset_pix2pix/latest_net_G.pth')
-model.eval()
-predictions = []
+    if opt.eval:
+        model.eval()
+    for i, data in enumerate(dataset):
+        if i >= opt.num_test:  # only apply our model to opt.num_test images.
+            break
+        model.set_input(data)  # unpack data from data loader
+        model.test()           # run inference
+        visuals = model.get_current_visuals()  # get image results
+        img_path = model.get_image_paths()     # get image paths
+        
+        print('processing (%04d)-th image... %s' % (i, img_path))
+        save_images(webpage, visuals, img_path, aspect_ratio=opt.aspect_ratio, width=opt.display_winsize, use_wandb=opt.use_wandb)
 
-with torch.no_grad():
-    for gray_imgs, img_names in test_loader:
-        gray_imgs = gray_imgs.to('cuda')
-        outputs = model(gray_imgs)
-        outputs = outputs.cpu().numpy()
-        predictions.append(outputs)
 
-predictions = np.concatenate(predictions, axis=0)
-predictions = (predictions * 255).astype(np.uint8)
+        
+        # Convert the visuals to a PIL image and then to a numpy array
+        for label, image in visuals.items():
+            
+            if label == 'fake_B_rgb':  # Assuming 'fake_B_rgb' is the generated image
+                if isinstance(image, np.ndarray):  # Check if the image is a numpy array
+                    # Ensure the image is in HWC format and values are in the range 0-255
+                    if  image.shape[0]==3: # CHW foramt 
+                    	image = image.transpose(1,2,0)
+                    if np.all((image >=0) & (image < 1)): # 0<rgb.value<1 
+                    	image = image * 225
+                    image = (image).astype(np.uint8)  # Ensure values are uint8
+                    image_pil = Image.fromarray(image)
+                else:
+                    print(f"Unsupported image type: {type(image)}")
+                    continue
 
-# 保存预测结果
-np.save("prediction.npy", predictions)
+                image_resized = image_pil.resize((224, 224))  # Resize image to 224x224
+                image_np = np.array(image_resized, dtype=np.uint8)
+                output_images.append(image_np)
+                
+
+    webpage.save()  # save the HTML
+
+    # Ensure we have exactly 50 images
+    if len(output_images) != 50:
+        print(f'Error: Expected 50 images, but got {len(output_images)}')
+    else:
+        # Convert list to numpy array and save it
+        output_array = np.stack(output_images)
+        np.save("prediction.npy", output_array)
+        print('Saved prediction.npy successfully')
+
